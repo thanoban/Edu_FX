8c0fa9b6a2c81ac62b36f80d038125e789e8a860
thanoban <thanobansk@gmail.com>
2026-03-09 09:59:47 +0530

    auth done

--- backend/auth/router.py
+++ backend/auth/router.py
@@ -1,3 +1,29 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from auth.service import register_student, login_student
+from utils.response import success_response, error_response
 
 router = APIRouter()
+
+class RegisterRequest(BaseModel):
+    name: str
+    email: str
+    password: str
+
+class LoginRequest(BaseModel):
+    email: str
+    password: str
+
+@router.post("/register")
+def register(req: RegisterRequest):
+    data, error = register_student(req.name, req.email, req.password)
+    if error:
+        return error_response(error)
+    return success_response("Student registered successfully", data)
+
+@router.post("/login")
+def login(req: LoginRequest):
+    data, error = login_student(req.email, req.password)
+    if error:
+        return error_response(error)
+    return success_response("Login successful", data)
